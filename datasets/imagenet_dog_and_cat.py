7862a4670856b973c1b2918c98a0637abf39804b
Ryohei Suzuki <quolc.i@gmail.com>
2018-11-28 15:38:10 +0900

    add datasets/ imagenet_dog_and_cat*

---
+++ datasets/imagenet_dog_and_cat.py
@@ -0,0 +1,111 @@
+import sys
+import numpy as np
+import chainer
+import random
+import scipy.misc
+
+classes_dog_and_cat = np.arange(151, 294, dtype=np.int32)
+class_to_index = dict()
+for i, c in enumerate(classes_dog_and_cat):
+    class_to_index[c] = i
+
+
+class ImageNetDogAndCatDataset(chainer.dataset.DatasetMixin):
+    def __init__(self, path, root, size=128, resize_method='bilinear', augmentation=False, crop_ratio=0.9):
+        self.base = chainer.datasets.LabeledImageDataset(path, root)
+        self.size = size
+        self.resize_method = resize_method
+        self.augmentation = augmentation
+        self.crop_ratio = crop_ratio
+
+    def __len__(self):
+        return len(self.base)
+
+    def transform(self, image):
+        c, h, w = image.shape
+        if c == 1:
+            image = np.concatenate([image, image, image], axis=0)
+        short_side = h if h < w else w
+        if self.augmentation:
+            crop_size = int(short_side * self.crop_ratio)
+            # Randomly crop a region and flip the image
+            top = np.random.randint(0, h - crop_size - 1)
+            left = np.random.randint(0, w - crop_size - 1)
+            if np.random.randint(0, 1):
+                image = image[:, :, ::-1]
+        else:
+            crop_size = short_side
+            # Crop the center
+            top = (h - crop_size) // 2
+            left = (w - crop_size) // 2
+        bottom = top + crop_size
+        right = left + crop_size
+
+        image = image[:, top:bottom, left:right]
+        _, h, w = image.shape
+        image = scipy.misc.imresize(image.transpose(1, 2, 0),
+                                    [self.size, self.size], self.resize_method).transpose(2, 0, 1)
+        image = image / 128. - 1.
+        image += np.random.uniform(size=image.shape, low=0., high=1. / 128)
+        return image
+
+    def get_example(self, i):
+        image, label = self.base[i]
+        image = self.transform(image)
+        index = class_to_index[int(label)]
+        return image, index
+
+
+if __name__ == "__main__":
+    import glob, os, sys
+
+    root_path = sys.argv[1]
+    if len(sys.argv) == 4:
+        root_path_val = sys.argv[2]
+        anno_path = sys.argv[3]
+        preprocess_val = True
+    else:
+        preprocess_val = False
+
+    dirname_to_label = {}
+    with open('dirname_to_label.txt', 'r') as f:
+        for line in f:
+            dirname, label = line.strip('\n').split(' ')
+            dirname_to_label[dirname] = label
+
+    count = 0
+    n_image_list = []
+    filenames = glob.glob(root_path + '/*/*.JPEG')
+    for filename in filenames:
+        filename = filename.split('/')
+        dirname = filename[-2]
+        label = int(dirname_to_label[dirname])
+        if label in classes_dog_and_cat:
+            n_image_list.append([os.path.join(filename[-2], filename[-1]), label])
+            count += 1
+            if count % 10000 == 0:
+                print(count)
+    print("Num of examples:{}".format(count))
+    n_image_list = np.array(n_image_list, np.str)
+    np.savetxt('image_list_dog_and_cat.txt', n_image_list, fmt="%s")
+
+    if preprocess_val:
+        count = 0
+        n_image_list = []
+        import xml.etree.ElementTree as et
+        filenames = glob.glob(root_path_val + '/*.JPEG')
+        for filename in filenames:
+            filename = filename.split('/')
+            image_name = filename[-1].strip('.JPEG')
+            e = et.parse(os.path.join(anno_path, image_name+'.xml')).getroot()
+            dirname = e.findall('object')[0].findall('name')[0].text
+            label = int(dirname_to_label[dirname])
+            if label in classes_dog_and_cat:
+                n_image_list.append([os.path.join(filename[-1]), label])
+                count += 1
+                if count % 1000 == 0:
+                    print(count)
+        print("Num of examples:{}".format(count))
+        n_image_list = np.array(n_image_list, np.str)
+        np.savetxt('image_list_dog_and_cat_val.txt', n_image_list, fmt="%s")
+
---
+++ datasets/imagenet_dog_and_cat_enc.py
@@ -0,0 +1,104 @@
+import sys
+import numpy as np
+import chainer
+import random
+import scipy.misc
+
+
+classes_dog_and_cat = np.arange(151, 294, dtype=np.int32)
+class_to_index = dict()
+for i, c in enumerate(classes_dog_and_cat):
+    class_to_index[c] = i
+
+
+class ImageNetDogAndCatEncDataset(chainer.dataset.DatasetMixin):
+    def __init__(self, path_input, path_truth, root_input, root_truth, size=128, resize_method='bilinear', augmentation=False, crop_ratio=0.9):
+        self.base_input = chainer.datasets.LabeledImageDataset(path_input, root_input)
+        self.base_truth = chainer.datasets.LabeledImageDataset(path_truth, root_truth)
+        self.size = size
+        self.resize_method = resize_method
+        self.augmentation = augmentation
+        self.crop_ratio = crop_ratio
+
+    def __len__(self):
+        return len(self.base_input)
+
+    def transform(self, image1, image2):
+        image2 = scipy.misc.imresize(image2.transpose(1, 2, 0),
+                                    [image1.shape[1], image1.shape[2]], self.resize_method).transpose(2, 0, 1)
+
+        _, h, w = image1.shape
+        if image1.shape[0] == 1:
+            image1 = np.concatenate([image1, image1, image1], axis=0)
+        if image2.shape[0] == 1:
+            image2 = np.concatenate([image2, image2, image2], axis=0)
+        if image1.shape != image2.shape:
+            raise Exception("input and truth image mismatch")
+
+        short_side = h if h < w else w
+        if self.augmentation:
+            crop_size = int(short_side * self.crop_ratio)
+            # Randomly crop a region and flip the image
+            top = random.randint(0, h - crop_size - 1)
+            left = random.randint(0, w - crop_size - 1)
+            if random.randint(0, 1):
+                image1 = image1[:, :, ::-1]
+                image2 = image2[:, :, ::-1]
+        else:
+            crop_size = short_side
+            # Crop the center
+            top = (h - crop_size) // 2
+            left = (w - crop_size) // 2
+        bottom = top + crop_size
+        right = left + crop_size
+
+        image1 = image1[:, top:bottom, left:right]
+        image2 = image2[:, top:bottom, left:right]
+        _, h, w = image1.shape
+        image1 = scipy.misc.imresize(image1.transpose(1, 2, 0),
+                                    [self.size, self.size], self.resize_method).transpose(2, 0, 1)
+        image2 = scipy.misc.imresize(image2.transpose(1, 2, 0),
+                                    [self.size, self.size], self.resize_method).transpose(2, 0, 1)
+        image1 = image1 / 128. - 1.
+        image2 = image2 / 128. - 1.
+        if self.augmentation:
+            image1 += np.random.uniform(size=image1.shape, low=0., high=1. / 128)
+            image2 += np.random.uniform(size=image1.shape, low=0., high=1. / 128)
+        return image1, image2
+
+
+    def get_example(self, i):
+        image_input, label = self.base_input[i]
+        image_truth, _ = self.base_truth[i]
+        image_input, image_truth = self.transform(image_input, image_truth)
+        index = class_to_index[int(label)]
+        return image_input, image_truth, index
+
+
+# generate image_list_dog_and_cat.txt by running this script:
+# python imagenet_dog_and_cat_enc.py IMAGE_ROOT_PATH
+if __name__ == "__main__":
+    import glob, os, sys
+
+    root_path = sys.argv[1]
+
+    dirname_to_label = {}
+    with open('dirname_to_label.txt', 'r') as f:
+        for line in f:
+            dirname, label = line.strip('\n').split()
+            dirname_to_label[dirname] = label
+
+    count = 0
+    n_image_list = []
+    filenames = glob.glob(root_path + '/*/*.jpg')
+    for filename in filenames:
+        filename = filename.split('/')
+        dirname = filename[-2]
+        label = int(dirname_to_label[dirname])
+        n_image_list.append([os.path.join(filename[-2], filename[-1]), label])
+        count += 1
+        if count % 10000 == 0:
+            print(count)
+    print("Num of examples:{}".format(count))
+    n_image_list = np.array(n_image_list, np.str)
+    np.savetxt('image_list_dog_and_cat.txt', n_image_list, fmt="%s")
